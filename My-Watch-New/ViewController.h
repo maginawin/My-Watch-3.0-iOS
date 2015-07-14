094664341850a407d56f7beb2edd61db7490d945
maginawin <maginawin@163.com>
2015-07-14 16:34:19 +0800

    Initial Commit

---
+++ My-Watch-New/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My-Watch-New
+//
+//  Created by maginawin on 15/7/14.
+//  Copyright (c) 2015年 wendong wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ My-Watch-NewTests/My_Watch_NewTests.m
@@ -0,0 +1,40 @@
+//
+//  My_Watch_NewTests.m
+//  My-Watch-NewTests
+//
+//  Created by maginawin on 15/7/14.
+//  Copyright (c) 2015年 wendong wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface My_Watch_NewTests : XCTestCase
+
+@end
+
+@implementation My_Watch_NewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
